0322de062473610a2656480b20d8c5f7777d827e
yildiz ucar <yildizucr@gmail.com>
2021-07-01 02:08:15 -0400

    added more hello feature

--- src/main/java/HelloJune.java
+++ src/main/java/HelloJune.java
@@ -2,7 +2,8 @@ public class HelloJune {
     public static void main(String[] args) {
         System.out.println("HelloJune");
         System.out.println("Developing hello feature");
-
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
     }
 
 }
