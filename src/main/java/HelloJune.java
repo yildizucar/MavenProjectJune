d3d40ab31807f583c53e8928687449a0b6dc0a6d
yildiz ucar <yildizucr@gmail.com>
2021-07-01 00:49:17 -0400

    started hello feature

--- src/main/java/HelloJune.java
+++ src/main/java/HelloJune.java
@@ -1,6 +1,8 @@
 public class HelloJune {
     public static void main(String[] args) {
         System.out.println("HelloJune");
+        System.out.println("Developing hello feature");
+
     }
 
 }
